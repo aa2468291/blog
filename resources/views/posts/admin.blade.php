1ce12d061f24db3be789ed39b22264b7f6a06281
aa2468291 <aa2468291@gmail.com>
2019-07-12 11:48:13 +0800

    admin/create view updated

--- resources/views/posts/admin.blade.php
+++ resources/views/posts/admin.blade.php
@@ -7,11 +7,8 @@
                 <div class="col-md-12">
                     <h4 class="text-uppercase">Blog Admin Panel</h4>
                     <ol class="breadcrumb">
-                        <li><a href="/">Home</a>
-                        </li>
-                        <li class="active"><a href="/posts">Blog</a>
-                        </li>
-                        <li class="active">Blog Listing</li>
+                        <li><a href="/">Home</a></li>
+                        <li class="active">Blog Admin Panel</li>
                     </ol>
                 </div>
             </div>
@@ -22,7 +19,20 @@
 @section('content')
     <div class="page-content">
         <div class="container">
+
+            <div class="clearfix toolbox">
+                <a href="/posts/create" class="btn btn-primary pull-right">create post</a>
+            </div>
+
+{{--            <br class="hidden-xs"/>--}}
+
+
+
+
+
+
             <div class="list-group">
+
                 @foreach($posts as $key =>$post)
                     <a href="#" class="list-group-item">{{$post->title}}</a>
                 @endforeach
